159bb63439db1b9f8ccadce33e268fc46bf35c78
Elijah Chou <ecool7079@gmail.com>
2023-02-13 11:53:19 -0500

    update with completed UI

--- src/components/LandingSection.js
+++ src/components/LandingSection.js
@@ -1,10 +1,11 @@
 import React from "react";
 import { Avatar, Heading, VStack } from "@chakra-ui/react";
 import FullScreenSection from "./FullScreenSection";
+import headshot from "../images/headshot.png"
 
-const greeting = "Hello, I am Pete!";
-const bio1 = "A frontend developer";
-const bio2 = "specialised in React";
+const greeting = "Hello, my name is Elijah!";
+const bio1 = "An aspiring full-stack developer";
+const bio2 = "and a MS in CS candidate";
 
 // Implement the UI for the LandingSection component according to the instructions.
 // Use a combination of Avatar, Heading and VStack components.
@@ -15,6 +16,28 @@ const LandingSection = () => (
     isDarkBackground
     backgroundColor="#2A4365"
   >
+    <VStack spacing={16}>
+      <VStack spacing={4}>
+        <Avatar
+          src={headshot}
+          size="2xl"
+          name="Your name"
+        />
+        <Heading as="h4" size="md" noOfLines={1}>
+          {greeting}
+        </Heading>
+      </VStack>
+      <VStack spacing={6}>
+        <Heading as="h1" size="3xl" noOfLines={1}>
+          {bio1}
+        </Heading>
+      </VStack>
+      <VStack spacing={6}>
+        <Heading as="h1" size="3xl" noOfLines={1}>
+          {bio2}
+        </Heading>
+      </VStack>
+    </VStack>
 
   </FullScreenSection>
 );
